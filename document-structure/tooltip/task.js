b60ee9192c10148ac823fe6228d93b7e9ba30e84
smetanova-ys <smetanova.ys@gmail.com>
2020-01-14 15:23:27 +0100

    Second

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,6 +5,7 @@ let accentuatedPhrases = document.getElementsByClassName('has-tooltip');
 for (let phrase of accentuatedPhrases) {
     phrase.insertAdjacentHTML('afterend', 
     `<div class='tooltip'>${phrase.title}</div>`);
+    
     phrase.addEventListener('click', function(event) {
         event.preventDefault();
         let hiddenEl = event.target.nextElementSibling;
@@ -15,11 +16,13 @@ for (let phrase of accentuatedPhrases) {
         }
 
         let activeEl = document.getElementsByClassName('tooltip_active')[0];
+    
         if (activeEl) {
             activeEl.classList.toggle('tooltip_active');
         }
         hiddenEl.style.left = event.target.getBoundingClientRect().left + 'px';
         hiddenEl.style.top = event.target.getBoundingClientRect().top + 20 + 'px';
         hiddenEl.classList.toggle('tooltip_active');
+    
     });
 }
