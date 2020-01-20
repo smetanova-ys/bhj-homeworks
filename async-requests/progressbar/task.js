818b72b41253cc053f7b3968f650f52f1ef75db4
smetanova-ys <smetanova.ys@gmail.com>
2020-01-20 15:55:04 +0100

    Changes have been made for progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -7,22 +7,18 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     let formData = new FormData(form);
-
     let request = new XMLHttpRequest();
+
     request.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
 
     request.upload.onprogress = function (event) {
-        console.log(`Отправлено ${event.loaded} из ${event.total} байт`);
-        progress.value = event.loaded;
+        //console.log(`Отправлено ${event.loaded} из ${event.total} байт`);
+        progress.value = event.loaded / event.total;
     }
 
     request.upload.onerror = function() {
         console.log(`Произошла ошибка во время отправки: ${request.status}`);
-    }
-
-    request.onprogress = function(event) {
-        progress.value = event.loaded;
-    }
+    };
 
     request.send(formData);
 });
